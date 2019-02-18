e9cbeb33855e9a119afdfd5bdacec15e3df93709
Andrew <36339785+Botinok666@users.noreply.github.com>
2019-02-18 08:37:23 +0300

    Upload files v0.0

---
+++ src/main/java/com/sfedu/JMovie/JMovieApplication.java
@@ -0,0 +1,14 @@
+package com.sfedu.JMovie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JMovieApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JMovieApplication.class, args);
+	}
+
+}
+
